f6a2371d66cd676b2052901755665d80de5ed1e2
GustavoCaso <gustavocaso@gmail.com>
2017-09-15 18:37:27 +0200

    Added GET show api endpoint

--- app/routes/api/devices.rb
+++ app/routes/api/devices.rb
@@ -1,8 +1,18 @@
 get '/api/v1/devices' do
+  content_type :json
+
   Device.all.to_json
 end
 
+get '/api/v1/devices/:id' do
+  content_type :json
+
+  Device[params['id']].to_json
+end
+
 put '/api/v1/devices/:id' do
+  content_type :json
+
   device = Device[params['id']]
   client_data = JSON.parse(request.body.read.to_s)
   updated_data = Api::UpdateControlData.call(device.data, client_data)
